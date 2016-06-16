48d9ebbb59244f232d6de2f09b81e74fe9bc4616
shoubhik bose <sbose78@gmail.com>
2016-06-16 18:30:29 +0000

    added paramterization for internal_interface , external_interface , socks_port , client_pass_start_range , client_pass_end_range ,generic_pass_outgoing_start_range , generic_pass_incoming_end_range

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -1,6 +1,17 @@
 class dante::configure{
 
 
+	$internal_interface = $dante::internal_interface
+	$external_interface = $dante::external_interface
+	$socks_port = $dante::socks_port 
+	$client_pass_start_range = $dante::client_pass_start_range
+	$client_pass_end_range = $dante::client_pass_end_range
+	$generic_pass_outgoing_start_range = $dante::generic_pass_outgoing_start_range
+	$generic_pass_incoming_end_range = $dante::generic_pass_incoming_end_range
+
+
+	notify { " using global ${client_pass_start_range} ******************** ": }
+	
 	file { "/etc/sockd.conf":
                 source => "puppet:///modules/dante/dantedconf",
         }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,20 @@
-class dante( $version = '1.4.1' ){
+class dante( 
+	$version = '1.4.1',
+	$internal_interface = '0.0.0.0',
+	$external_interface = 'eth0',
+	$socks_port = '8001',
+	$client_pass_start_range = '0.0.0.0/0',
+	$client_pass_end_range = '0.0.0.0/0',
+	$generic_pass_outgoing_start_range = '0.0.0.0/0',
+	$generic_pass_incoming_end_range = '0.0.0.0/0'
+){
 
 
 
 	class{'dante::install': 
 		version => $version
 	} -> 
- 	class{'dante::configure': } ~> 
+ 	class{'dante::configure':} ~> 
 	class{'dante::service': } ->
 	Class['dante'] 
 
